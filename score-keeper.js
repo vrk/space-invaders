53c90a1c7a61b9f7886852a96977702df824a725
vrk <vrkwork@gmail.com>
2017-06-04 14:20:39 -0700

    add score keeper

---
+++ score-keeper.js
@@ -0,0 +1,18 @@
+const X_POSITION = CANVAS_WIDTH - 10;
+const Y_POSITION = 30;
+class ScoreKeeper {
+  constructor() {
+    this.score = 0;
+  }
+
+  update() {
+    this.score += 10;
+  }
+
+  render(ctx) {
+    ctx.fillStyle = 'blueviolet';
+    ctx.font = '20px Consolas, monospace';
+    ctx.textAlign = 'right';
+    ctx.fillText(`SCORE: ${this.score}`, X_POSITION, Y_POSITION);
+  }
+}
